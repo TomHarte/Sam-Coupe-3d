179c30757d825c4addcdaf18392398302b6c1342
Thomas Harte <thomas.harte@gmail.com>
2012-03-15 11:33:40 -0700

    Added some commenting, both to make the obvious point that this is the build script and to mention again some of the prerequisites and the logic behind the code library.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-../pyz80 -I ../SAMDOS -s blargh2 -s polyt -s drawloop -s himem src/lib3d/3d.z80s
+../pyz80 -I ../SAMDOS src/lib3d/3d.z80s
 open 3d.dsk
 
 # -o game.dsk  -I ../SAMDOS 
+#
+#	Very little to say here; this is just my script for compiling the project
+#	and launching it (with OS X's open command, which will do the same as if
+#	the DSK icon had been double clicked, i.e. since the .dsk was just created
+#	it'll load the default .dsk handler - Sim Coupe, probably).
+#
+#	I assume that one folder up from here is pyz80 and a binary image of SAMDOS.
+#	Neither is included, though both may be freely redistributed.
+#
+#	The library is set up so that you just need to compile 3d.z80s. It'll
+#	include your globals and your draw, init, IRQ and update code in the
+#	appropriate place and handles machine setup, memory paging, etc, itself.
+#
